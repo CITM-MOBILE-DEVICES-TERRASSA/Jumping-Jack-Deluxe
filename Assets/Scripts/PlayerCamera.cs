10aa5c47d69fc9068f2db95f093e93b52a590de8
eduardobr33 <ebergillosrivas@gmail.com>
2024-11-14 12:44:21 +0100

    Jump + Slide
    - Función Jump para saltar, se puede modificar con jumForce.
    -Función Slide para deslizarse, al activarse cambia el collider, se puede modificar la duración con slideDuration.
    - Reducción de velocidad mientras se esta deslizando.

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -16,15 +16,15 @@ public class PlayerCamera : MonoBehaviour
     private void Update()
     {
 
-        // Calcula la mitad del tamaño de la cámara en X e Y
-        cameraHalfHeight = Camera.main.orthographicSize;
-        cameraHalfWidth = cameraHalfHeight * Camera.main.aspect;
+        //// Calcula la mitad del tamaño de la cámara en X e Y
+        //cameraHalfHeight = Camera.main.orthographicSize;
+        //cameraHalfWidth = cameraHalfHeight * Camera.main.aspect;
 
-        // Calcula la posición deseada de la cámara en base a la posición del jugador
-        float targetX = Mathf.Clamp(player.position.x, minX + cameraHalfWidth, maxX - cameraHalfWidth);
-        float targetY = Mathf.Clamp(player.position.y, minY + cameraHalfHeight, maxY - cameraHalfHeight);
+        //// Calcula la posición deseada de la cámara en base a la posición del jugador
+        //float targetX = Mathf.Clamp(player.position.x, minX + cameraHalfWidth, maxX - cameraHalfWidth);
+        //float targetY = Mathf.Clamp(player.position.y, minY + cameraHalfHeight, maxY - cameraHalfHeight);
 
-        // Aplica la nueva posición de la cámara
-        transform.position = new Vector3(targetX, targetY, transform.position.z);
+        //// Aplica la nueva posición de la cámara
+        //transform.position = new Vector3(targetX, targetY, transform.position.z);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,74 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 10f;
+    public float slideDuration = 0.5f;
+    public float slideSpeed = 5f;          // Por si hay que reducir la velocidad al deslizarse
+
+    public Collider2D playerCollider;
+    public Collider2D slideCollider;
+
     private Vector2 direction = Vector2.right;
     private Rigidbody2D rb;
+
+    private bool isGrounded = false;
+    private bool isSliding = false;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        rb.velocity = direction * speed;
+        if (!isSliding)
+        {
+            rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
+        }
+        else rb.velocity = new Vector2(direction.x * slideSpeed, rb.velocity.y);
     }
     public void ChangeDirection()
     {
         direction = -direction;
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    public void Slide()
+    {
+        if(isGrounded && !isSliding)
+        {
+            StartCoroutine(SlideCoroutine());
+        }
+    }
+
+    IEnumerator SlideCoroutine()
+    {
+        isSliding = true;
+        playerCollider.enabled = false;
+        slideCollider.enabled = true;
+
+        yield return new WaitForSeconds(slideDuration);
+
+        playerCollider.enabled = true;
+        slideCollider.enabled = false;
+        isSliding = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
