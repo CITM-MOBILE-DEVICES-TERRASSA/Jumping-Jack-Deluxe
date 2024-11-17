a6fcdc3628b2fe65f0bac2b7ee9bbbd4419df05f
Eduuuuuuuuuuuu <eduard.garcia.tobajas@estudiantat.upc.edu>
2024-11-17 12:50:31 +0100

    Mecanica de valla hecha

---
+++ Assets/Scripts/DeadlyObstacle.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadlyObstacle : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
