42ffd22cfce196d05f3f615a59df9cc775f666b6
Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>
2024-11-25 09:48:36 +0100

    loading bar screen
    Co-Authored-By: Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>

--- Assets/Scripts/LoadScreen.cs
+++ Assets/Scripts/LoadScreen.cs
@@ -5,8 +5,7 @@ using System.Collections;
 
 public class SplashScreen : MonoBehaviour
 {
-    public Slider loadingBar;
-    public float loadingSpeed = 0.5f;
+    [SerializeField] private AnimationClip loadingBarAnim;
 
     private void Start()
     {
@@ -15,16 +14,7 @@ public class SplashScreen : MonoBehaviour
 
     private IEnumerator LoadLobbyScene()
     {
-        float progress = 0;
-
-        while (progress < 1f)
-        {
-            progress += Time.deltaTime * loadingSpeed;
-            loadingBar.value = progress;
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(loadingBarAnim.length);
 
         SceneManager.LoadScene("Lobby");
     }
