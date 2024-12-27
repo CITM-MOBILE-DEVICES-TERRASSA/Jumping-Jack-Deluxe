f8ccda6dbc2dba7889c3f17036a887e15a40a3d0
AlejandroIR <shibainuale@gmail.com>
2024-12-27 18:01:15 +0100

    AIR - Jumping jack Deluxe basic player movement
     (correr, deslizar, doble salto, cambio de direccion)

---
+++ Assets/JumpingJackDeluxe/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float smoothTime = 0.3f;
+    
+    [Header("Camera Size")]
+    public float normalSize = 5f;
+    public float jumpSize = 6f;
+    public float sizeChangeSpeed = 2f;
+
+    private PlayerMovement playerMovement;
+    private Vector3 velocity;
+    private Camera cam;
+
+    private void Start()
+    {
+        playerMovement = target.GetComponent<PlayerMovement>();
+        cam = GetComponent<Camera>();
+        cam.orthographicSize = normalSize;
+    }
+
+    private void LateUpdate()
+    {
+        // Position follow
+        Vector3 currentOffset = offset;
+        currentOffset.x = playerMovement.IsFacingRight ? offset.x : -offset.x;
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + currentOffset, ref velocity, smoothTime);
+
+        // Camera size adjustment
+        float targetSize = playerMovement.IsInAir ? jumpSize : normalSize;
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, sizeChangeSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/JumpingJackDeluxe/Scripts/PlayerMovement.cs
@@ -0,0 +1,163 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private bool canDoubleJump = true;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Slide")]
+    [SerializeField] private float slideSpeed = 15f;
+    [SerializeField] private float slideDuration = 0.5f;
+    [SerializeField] private float slideCooldown = 1f;
+    [SerializeField] private float slideYOffset = 0.5f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Vector3 originalScale;
+
+    private bool isFacingRight = true;
+    private bool hasDoubleJumped;
+    private bool isSliding;
+    private bool canSlide = true;
+    private float slideTimeLeft;
+    private float currentSlideSpeed;
+
+    public bool IsFacingRight => isFacingRight;
+    public bool IsInAir => !IsGrounded;
+    private bool IsGrounded => Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleSlide();
+    }
+
+    private void HandleMovement()
+    {
+        if (isSliding) return;
+
+        if (IsGrounded) hasDoubleJumped = false;
+        rb.velocity = new Vector2(moveSpeed * GetDirection(), rb.velocity.y);
+    }
+
+    private void HandleSlide()
+    {
+        if (!isSliding) return;
+
+        slideTimeLeft -= Time.deltaTime;
+        float slideProgress = 1 - (slideTimeLeft / slideDuration);
+        currentSlideSpeed = Mathf.Lerp(slideSpeed, moveSpeed, slideProgress);
+        rb.velocity = new Vector2(currentSlideSpeed * GetDirection(), rb.velocity.y);
+
+        if (slideTimeLeft <= 0) EndSlide();
+    }
+
+    public void OnJumpButtonPressed()
+    {
+        if (IsGrounded)
+        {
+            Jump();
+        }
+        else if (canDoubleJump && !hasDoubleJumped)
+        {
+            DoubleJump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void DoubleJump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        Jump();
+        hasDoubleJumped = true;
+    }
+
+    public void OnSlideButtonPressed()
+    {
+        if (canSlide && !isSliding && IsGrounded) StartSlide();
+    }
+
+    private void StartSlide()
+    {
+        isSliding = true;
+        canSlide = false;
+        slideTimeLeft = slideDuration;
+        currentSlideSpeed = slideSpeed;
+        
+        rb.velocity = new Vector2(slideSpeed * GetDirection(), rb.velocity.y);
+        UpdatePosition(slideYOffset, true);
+        StartCoroutine(SlideCooldown());
+    }
+
+    private void EndSlide()
+    {
+        isSliding = false;
+        UpdatePosition(slideYOffset, false);
+    }
+
+    private void UpdatePosition(float offset, bool isStarting)
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y += isStarting ? -offset : offset;
+        transform.position = newPosition;
+        transform.localScale = new Vector3(transform.localScale.x, isStarting ? 1f : 2f, transform.localScale.z);
+    }
+
+    public void OnFlipButtonPressed() => Flip();
+
+    private float GetDirection()
+    {
+        return isFacingRight ? 1f : -1f;
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        FlipChildSprites();
+    }
+
+    //FUNCIÓN PARA FLIPEAR LA GORRA - BORRAR SI EL SPRITE ORIGINAL NO TIENE HIJOS
+    private void FlipChildSprites()
+    {
+        foreach (var sprite in GetComponentsInChildren<SpriteRenderer>())
+        {
+            if (sprite != spriteRenderer) sprite.flipX = !isFacingRight;
+        }
+    }
+
+    private IEnumerator SlideCooldown()
+    {
+        yield return new WaitForSeconds(slideCooldown);
+        canSlide = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
